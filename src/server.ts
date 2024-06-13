13abf90965c0a2df62749496f11b52227443dae6
Breno Alves <breno7572@gmail.com>
2024-06-13 13:10:26 -0300

    Configuração inicial

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', (request, reply) => {
+  reply.send({ message: 'Hello, World' })
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server is running on port 3333')
+  })
